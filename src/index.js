52d97abe0be03304ea3c958e97215c435434bc65
xinpu <xinpuchen@126.com>
2019-12-21 23:52:40 +0800

    refactor: 重构部分代码

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './container';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import "babel-polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import configureStore from "./store";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import "./index.css";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
+
 registerServiceWorker();
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import appReducer from '../reducer'
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import appReducer from "../reducer";
 
 const loggerMiddleware = createLogger();
 
-export default function configureStore(preloadedState){
+export default function configureStore(preloadedState) {
   return createStore(
     appReducer,
     preloadedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
-    )
-  )
+    applyMiddleware(thunkMiddleware, loggerMiddleware)
+  );
 }
